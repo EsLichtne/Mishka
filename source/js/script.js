c29c77f097baea28718375630a9b940aab7c1ee9
Ксения <easyghoul@gmail.com>
2023-09-26 15:31:19 +0700

    Добавляет стилизацию шапки

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,12 @@
 let navButton = document.querySelector(".navigation__button");
 let toggle = document.querySelector(".navigation__toggle");
-let menu = document.querySelector(".navigation__menu");
-// let siteList = document.querySelector(".site-list");
-// let userList = document.querySelector(".user-list");
+let nav = document.querySelector(".navigation");
 let navClue = document.querySelector(".navigation__clue");
 
 navButton.addEventListener("click", function () {
   toggle.classList.toggle("navigation__toggle--open");
-  menu.classList.toggle("navigation__menu--open");
-  if (menu.classList.contains("navigation__menu--open")) {
+  nav.classList.toggle("navigation--open");
+  if (nav.classList.contains("navigation--open")) {
     navClue.textContent = "Закрыть меню.";
   } else {
     navClue.textContent = "Открыть меню.";
