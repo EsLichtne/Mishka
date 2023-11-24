18c6c0687e6582e25a50cb398bf1fb78d0f12172
Ксения <easyghoul@gmail.com>
2023-11-24 11:22:12 +0700

    Исправляет кавычки на одинарные

---
+++ source/js/navigation.js
@@ -0,0 +1,14 @@
+let navButton = document.querySelector('.navigation__button');
+let toggle = document.querySelector('.navigation__toggle');
+let nav = document.querySelector('.navigation');
+let navClue = document.querySelector('.navigation__clue');
+
+navButton.addEventListener('click', function () {
+  toggle.classList.toggle('navigation__toggle--open');
+  nav.classList.toggle('navigation--open');
+  if (nav.classList.contains('navigation--open')) {
+    navClue.textContent = 'Закрыть меню.';
+  } else {
+    navClue.textContent = 'Открыть меню.';
+  }
+})
